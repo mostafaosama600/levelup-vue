34278dc308d8e17721591814e0a08b697527a383
mostafa osama <mostafaelshrombably775@gmail.com>
2023-08-25 16:26:54 +0300

    @add tailwindCSS

---
+++ src/components/Products.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="bg-black p-1 text-white rounded">Hello World</button>
+</template>
+<script>
+export default {
+  name: "Products",
+  data() {
+    return {};
+  },
+};
+</script>
